c3ed1079ce13efbd266e956e923ee465dc49bed2
Evan Dempsey <evangd@umich.edu>
2023-12-10 16:33:09 -0500

    We've got ourselves a working game

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const Gameboard = (function() {
     const board = Array(9).fill(' ');
 
-    const getBoard = () => {
+    const markBoard = () => {
         const cells = document.querySelectorAll('button');
         for (let i = 0; i < cells.length; ++i) {
             cells[i].textContent = board[i];
         }
     };
 
-    const markBoard = (cell, player) => {
+    const takeTurn = (cell, player) => {
         board[cell] = player.getLetter();
-        getBoard();
+        markBoard();
         return checkResult(cell, player);
     };
 
     function checkResult(cell, player) {
-        if (checkRow(cell) || checkCol(cell)) {
-            console.log(player.getName() + ' wins!');
+        console.log('checkDiag returns: ' + checkDiag(cell));
+        if (checkRow(cell) || checkCol(cell) || checkDiag(cell)) {
             return true;
         } else {
             return false;
@@ -46,6 +46,7 @@ const Gameboard = (function() {
     }
 
     function checkCol(cell) {
+        console.log(cell);
         if (cell === 0 || cell === 1 || cell === 2) {
             if (board[cell] === board[cell + 3] && board[cell] === board[cell + 6]) {
                 return true;
@@ -67,7 +68,23 @@ const Gameboard = (function() {
         }
     }
 
-    return {getBoard, markBoard};
+    function checkDiag(cell) {
+        switch (cell) {
+            case 0:
+                return (board[cell] === board[4] && board[cell] === board[8]) ? true : false;
+            case 2:
+                return (board[cell] === board[4] && board[cell] === board[6]) ? true : false;
+            case 4:
+                return (board[cell] === board[0] && board[cell] === board[8])
+                 || (board[cell] === board[6] && board[cell] === board[2]) ? true : false;
+            case 6:
+                return board[cell] === board[4] && board[cell] === board[2] ? true : false;
+            case 8:
+                return board[cell] === board[4] && board[cell] === board[0] ? true : false;
+        }
+    }
+
+    return {markBoard, takeTurn};
 })();
 
 const Game = (function() {
@@ -80,16 +97,20 @@ const Game = (function() {
 
         waitForClick.then((result) => {
             
-            if (!Gameboard.markBoard(result, p1)) {
+            if (!Gameboard.takeTurn(result, p1)) {
 
                 let waitForClick2 = new Promise(addClicks);
         
                 waitForClick2.then((result2) => {
-                    if (!Gameboard.markBoard(result2, p2)) {
+                    if (!Gameboard.takeTurn(result2, p2)) {
                         playRound();
+                    } else {
+                        declareWinner(p2);
                     }
                 });
 
+            } else {
+                declareWinner(p1);
             }
         });
     };
@@ -97,10 +118,24 @@ const Game = (function() {
     function addClicks(resolve, reject) {
         const squares = document.querySelectorAll('button');
         for (let i = 0; i < squares.length; ++i) {
-            squares[i].addEventListener('click', function(e) {
-                resolve(e.target.dataset.coord);
-            });
+            if (squares[i].textContent === ' ') {
+                squares[i].addEventListener('click', function(e) {
+                    resolve(Number(e.target.dataset.coord));
+                });
+            }
         }
+    }
+
+    function declareWinner(player) {
+        const banner = document.createElement('div');
+        const message = document.createElement('h2');
+        const playAgain = document.createElement('button');
+        message.textContent = player.getName() + ' wins!';
+        playAgain.textContent = 'Play Again';
+        banner.classList.add('banner');
+        banner.appendChild(message);
+        banner.appendChild(playAgain);
+        document.body.appendChild(banner);
     }
 
     return {playRound};
